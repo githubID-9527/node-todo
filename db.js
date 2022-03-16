66e6ebec192c9d738bcb8047ef3417594c017340
haha <1732976075@qq.com>
2022-03-16 23:57:12 +0800

    first commit

---
+++ db.js
@@ -0,0 +1,33 @@
+const homedir = require("os").homedir()
+const home = process.env.HOME || homedir
+const p = require("path")
+const fs = require("fs")
+const dbPath = p.join(home, ".todo")
+
+const db = {
+    read(path = dbPath){
+        return new Promise((resolve, reject) => {
+            fs.readFile(path, {flag: "a+"}, (err, data) => {
+                if(err) return reject(err)
+                let list
+                try {
+                    list = JSON.parse(data.toString())
+                } catch (err2) {
+                    list = []
+                }
+                resolve(list)
+            })
+        })
+    },
+    write(list, path = dbPath){
+        return new Promise((resolve, reject) => {
+            const string = JSON.stringify(list)
+            fs.writeFile(path, string + "\n", (err) => {
+                if(err) return reject(err)
+                resolve()
+            })
+        })
+    }
+}
+
+module.exports = db
